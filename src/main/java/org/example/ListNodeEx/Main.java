02d9b6d7e485a190577b8acb77f201e45159400c
Dominika Windows <d0mi04@windows.com>
2025-07-11 16:01:59 +0200

    example of addNumbers usage in Main

--- src/main/java/org/example/ListNodeEx/Main.java
+++ src/main/java/org/example/ListNodeEx/Main.java
@@ -3,14 +3,10 @@ package org.example.ListNodeEx;
 public class Main {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 298, 3, 88, 5, 6, 7, 8, 9};
-        ListNode head = new ListNode(nums[0]);
-        ListNode tail = head;
-
-        for(int i = 1; i < nums.length; i++) {
-            tail.next = new ListNode(nums[i]);
-            tail = tail.next;
-        }
-
-        ListNode.printList(head);
+        ListNode head = ListNode.makeLinkedListFromTab(nums);
+        ListNode num1 = ListNode.makeLinkedListFromTab(new int[]{9,9});
+        ListNode num2 = ListNode.makeLinkedListFromTab(new int[]{9, 9, 9, 9});
+//        ListNode.printList(head);
+        ListNode.addNumbers(num1, num2);
     }
 }
